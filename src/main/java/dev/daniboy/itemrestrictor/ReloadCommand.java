664ba67811591b8b0ea8d0533342f296550372f5
Daniboy <63450929+Daniboy0@users.noreply.github.com>
2024-02-20 05:24:03 +0200

    Update ReloadCommand.java

--- src/main/java/dev/daniboy/itemrestrictor/ReloadCommand.java
+++ src/main/java/dev/daniboy/itemrestrictor/ReloadCommand.java
@@ -1,2 +1,39 @@
-package dev.daniboy.itemrestrictor;public class ReloadCommand {
+package dev.daniboy.itemrestrictor;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+public class ReloadCommand implements CommandExecutor {
+
+	private final Plugin plugin;
+
+	public ReloadCommand(Plugin plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (label.equalsIgnoreCase("itemrestrictor") && args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+			if (sender.hasPermission("itemrestrictor.reloadconfig")) {
+				plugin.reloadConfig();
+				String reloadMessage = getConfigMessage("messages.reload_success");
+				sender.sendMessage(reloadMessage);
+				return true;
+			} else {
+				sender.sendMessage(getConfigMessage("messages.no_permission"));
+				return false;
+			}
+		} else {
+			sender.sendMessage(getConfigMessage("messages.reload_command_usage"));
+			return false;
+		}
+	}
+
+	private String getConfigMessage(String path) {
+		String message = plugin.getConfig().getString(path);
+		return ChatColor.translateAlternateColorCodes('&', message);
+	}
 }
