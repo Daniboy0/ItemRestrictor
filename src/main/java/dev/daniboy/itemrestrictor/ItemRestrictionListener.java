bb7c4dcdc0090aa0a9b338a55b8c69247d2be6fd
Daniboy <63450929+Daniboy0@users.noreply.github.com>
2024-02-20 05:23:39 +0200

    Update ItemRestrictionListener.java

--- src/main/java/dev/daniboy/itemrestrictor/ItemRestrictionListener.java
+++ src/main/java/dev/daniboy/itemrestrictor/ItemRestrictionListener.java
@@ -1,2 +1,36 @@
-package dev.daniboy.itemrestrictor;public class ItemRestrictionListener {
+package dev.daniboy.itemrestrictor;
+
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+
+public class ItemRestrictionListener implements Listener {
+
+	private final Plugin plugin;
+
+	public ItemRestrictionListener(Plugin plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		World world = player.getWorld();
+		String worldName = world.getName();
+
+		// Check if the player is using a restricted item
+		if (event.getItem() != null && isItemRestricted(event.getItem().getType().toString(), worldName)) {
+			String message = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("worlds." + worldName + ".restriction_message"));
+			player.sendMessage(message);
+			event.setCancelled(true); // Cancel the event to prevent usage
+		}
+	}
+
+	private boolean isItemRestricted(String item, String world) {
+		return plugin.getConfig().getStringList("worlds." + world + ".restricted_items").contains(item);
+	}
 }
